ab3830491a946f6a15aac16885b11cb2292ad7ae
Sam <sam.sutcliffe2@gmail.com>
2023-08-01 14:41:21 +0100

    Updated package def for re-import shadowing

--- src/extensions/simd/package.lisp
+++ src/extensions/simd/package.lisp
@@ -3,16 +3,16 @@
         #:magicl
         )
   ;; XXX: This is kind of annoying...
-  ;; (:shadowing-import-from #:magicl
-  ;;                         #:vector
-  ;;                         #:=
-  ;;                         #:map
-  ;;                         #:trace
-  ;;                         #:every
-  ;;                         #:some
-  ;;                         #:notevery
-  ;;                         #:notany
-  ;;                         #:make-array)
+  (:shadowing-import-from #:magicl
+                          #:vector
+                          #:=
+                          #:map
+                          #:trace
+                          #:every
+                          #:some
+                          #:notevery
+                          #:notany
+                          #:make-array)
   )
 
 (magicl:define-backend :simd
